fd83ef841bc93fd34307d118c0b118c3b1143005
Ariel Pellegrino Iborra <ariel.iborra@sensedia.com>
2023-05-29 20:51:51 -0300

    comentários

--- src/Aula2.java
+++ src/Aula2.java
@@ -1,4 +1,17 @@
 package JavaDeveloperDio.src;
 
+//Calculadora
 public class Aula2 {
+    /**
+     * @author Ariel Pellegrino
+     * @version 1.0
+     * @since 29/05/2023
+     * Este método é utilizado para somar dois números inteiros.
+     * @param numeroUm este é o primeiro parametro do método.
+     * @param numeroDois este é o segundo parametro do método.
+     * @return int o resultado deste método é a soma dos dois numeros.
+     */
+    public int somar (int numeroUm, int numeroDois){
+        return numeroUm + numeroDois;
+    }
 }
