f7a8265a6d48dd62f3eb76a2247b0c17318710d6
Ariel Pellegrino Iborra <ariel.iborra@sensedia.com>
2023-06-01 20:49:13 -0300

    FOR

--- src/Aula7.java
+++ src/Aula7.java
@@ -2,6 +2,10 @@ package JavaDeveloperDio.src;
 
 public class Aula7 {
     public static void main(String[] args) {
+            String[] alunos = {"Felipe", "Jonas", "Julia", "Marcos"};
 
+            for(int x = 0; x < alunos.length; x++){
+                System.out.println("O aluno no indice x=" + x + "é" + alunos[x]);
+            }
     }
 }
