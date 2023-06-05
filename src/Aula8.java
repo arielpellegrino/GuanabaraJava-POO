7a147464bff25b1767815492bf1391d674a6ec16
Ariel Pellegrino Iborra <ariel.iborra@sensedia.com>
2023-06-05 20:30:27 -0300

    break and continue

--- src/Aula8.java
+++ src/Aula8.java
@@ -1,4 +1,11 @@
 package JavaDeveloperDio.src;
 
 public class Aula8 {
+    public static void main(String[] args) {
+        String[] alunos = {"Felipe", "Jonas", "Julia", "Marcos"};
+
+        for(String aluno : alunos) {
+            System.out.println("Nome do aluno é:" + aluno);
+        }
+    }
 }
