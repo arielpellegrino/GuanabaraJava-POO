04a759e21bbffa58e306f9d35155842fa1338e9a
Ariel Pellegrino Iborra <ariel.iborra@sensedia.com>
2023-06-05 20:29:47 -0300

    break and continue

--- src/Aula9.java
+++ src/Aula9.java
@@ -1,4 +1,12 @@
 package JavaDeveloperDio.src;
 
 public class Aula9 {
+    public static void main(String[] args) {
+        for(int numero = 1; numero <=5; numero ++){
+            if (numero ==3)
+                continue;
+                System.out.println(numero);
+
+        }
+    }
 }
